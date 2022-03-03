be3f6ca8542a598f2f9ffe7cc4cf0baa86434392
admin <admin@Bcalab3-m23>
2022-03-03 11:08:26 +0530

    commit

--- gitProject/src/gitProject/Student.java
+++ gitProject/src/gitProject/Student.java
@@ -1,10 +1,15 @@
 package gitProject;
+import java.util.*;
 
 public class Student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String s;
+		Scanner sc= new Scanner (System.in);
+		
 		System.out.println("hello Student");
+		s=sc.next();
 
 	}
 
